ace24628785e1664fd3bc2c20a68a0fa986f2343
mrzcyber <mrzcyber@gmail.com>
2026-07-01 14:29:32 +0700

    feat(database): add restaurant orders migration

---
+++ database/migrations/2026_07_01_050731_create_restaurant_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('restaurant_orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('order_code')->unique();
+            $table->foreignId('tables_id')->nullable()->constrained('tables')->nullOnDelete();
+            $table->unsignedInteger('total_price');
+            $table->enum('status', ['pending', 'success', 'failed'])->default('pending');
+            $table->enum('payment', ['offline', 'online'])->default('online');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('restaurant_orders');
+    }
+};
