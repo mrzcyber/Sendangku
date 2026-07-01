e98f745f55a8f845dbf7ff50aff204d8d78c84ff
mrzcyber <mrzcyber@gmail.com>
2026-07-01 14:29:31 +0700

    feat(database): add ticket order items migration

---
+++ database/migrations/2026_07_01_050724_create_order_items_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained('orders')->cascadeOnDelete();
+            $table->foreignId('ticket_type_id')->nullable()->constrained('ticket_types')->nullOnDelete();
+            $table->unsignedInteger('qty');
+            $table->unsignedInteger('price');
+            $table->unsignedInteger('subtotal');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_items');
+    }
+};
