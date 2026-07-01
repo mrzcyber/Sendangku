8fbf6f2f0be697592e3bf97559e573aaca44529f
mrzcyber <mrzcyber@gmail.com>
2026-07-01 14:29:31 +0700

    feat(database): add service packages migration

---
+++ database/migrations/2026_07_01_050727_create_service_packages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('service_packages', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('service_id')->constrained('services')->cascadeOnDelete();
+            $table->string('title');
+            $table->unsignedInteger('price');
+            $table->text('benefit');
+            $table->string('whatsapp_message');
+            $table->string('whatsapp_number');
+            $table->boolean('populer')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('service_packages');
+    }
+};
