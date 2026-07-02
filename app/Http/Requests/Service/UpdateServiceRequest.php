1a2fefa7486b77431672f0871abd864bc4524cb1
mrzcyber <mrzcyber@gmail.com>
2026-07-02 11:31:46 +0700

    feat(services): add form requests

---
+++ app/Http/Requests/Service/UpdateServiceRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Service;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateServiceRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['sometimes', 'required', 'string', 'max:255'],
+            'description' => ['sometimes', 'required', 'string'],
+            'price' => ['sometimes', 'required', 'integer', 'min:0'],
+            'duration' => ['sometimes', 'required', 'string', 'max:255'],
+            'thumbnail' => ['sometimes', 'required', 'string', 'max:255'],
+        ];
+    }
+}
