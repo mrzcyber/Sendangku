1f703bb8f3dd41d4b413243a665bd703b54563cc
mrzcyber <mrzcyber@gmail.com>
2026-07-02 11:31:15 +0700

    feat(restaurant-menus): add form requests

---
+++ app/Http/Requests/RestaurantMenu/UpdateRestaurantMenuRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\RestaurantMenu;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRestaurantMenuRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'status' => ['sometimes', 'required', 'boolean'],
+            'name' => ['sometimes', 'required', 'string', 'max:255'],
+            'thumbnail' => ['sometimes', 'required', 'string', 'max:255'],
+            'price' => ['sometimes', 'required', 'integer', 'min:0'],
+        ];
+    }
+}
