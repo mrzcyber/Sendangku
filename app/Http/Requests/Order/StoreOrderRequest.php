9d64da735cf675bb53b17abf609a34b6b5dd7caf
mrzcyber <mrzcyber@gmail.com>
2026-07-02 11:31:02 +0700

    feat(orders): add form requests

---
+++ app/Http/Requests/Order/StoreOrderRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Order;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreOrderRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'buyer_name' => ['required', 'string', 'max:255'],
+            'buyer_phone' => ['required', 'string', 'max:255'],
+            'buyer_email' => ['required', 'email', 'max:255'],
+        ];
+    }
+}
---
+++ app/Http/Requests/Order/UpdateOrderRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\Order;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateOrderRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+        'qr_token' => ['required', 'uuid'],
+        ];
+    }
+}
