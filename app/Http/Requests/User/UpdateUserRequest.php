8509ceedcf468f07328bc7913cbd074ed8ad95aa
mrzcyber <mrzcyber@gmail.com>
2026-07-02 11:32:32 +0700

    feat(users): add form requests

---
+++ app/Http/Requests/User/UpdateUserRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['sometimes', 'required', 'string', 'max:255'],
+            'email' => [
+                'sometimes',
+                'required',
+                'email',
+                'max:255',
+                Rule::unique('users', 'email')->ignore($this->route('user')),
+            ],
+            'password' => ['sometimes', 'required', 'string', 'min:8'],
+            'role' => ['sometimes', 'required', 'string', 'in:admin,tiket,kasir'],
+        ];
+    }
+}
