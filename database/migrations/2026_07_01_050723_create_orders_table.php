5d0ac2771f82a9684250f3cd323d709c41066435
mrzcyber <mrzcyber@gmail.com>
2026-07-15 09:52:50 +0700

    fix(order):change to nullable in schema

--- database/migrations/2026_07_01_050723_create_orders_table.php
+++ database/migrations/2026_07_01_050723_create_orders_table.php
@@ -15,10 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('order_code')->unique();
             $table->uuid('qr_token')->unique();
-            $table->string('buyer_name');
-            $table->string('buyer_phone');
-            $table->string('buyer_email');
-            $table->unsignedInteger('total_price');
+            $table->string('buyer_name')->nullable();
+            $table->string('buyer_phone')->nullable();
+            $table->string('buyer_email')->nullable();
+            $table->unsignedInteger('total_price')->default(0);
             $table->enum('status', ['active', 'used'])->default('active');
             $table->enum('purchase', ['online', 'offline']);
             $table->timestamp('scanned_at')->nullable();
